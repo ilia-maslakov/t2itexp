2e52929965d384a30a7626a99161529ccff2370f
Ilia Maslakov <il.smind@gmail.com>
2022-11-29 06:50:20 +0300

    Update Task2.sql

--- sql/Task2.sql
+++ sql/Task2.sql
@@ -1,3 +1,43 @@
+-- Table: public.clients
+
+-- DROP TABLE IF EXISTS public.clients;
+
+CREATE TABLE IF NOT EXISTS public.clients
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 399999999 CACHE 1 ),
+    client_name character varying(200) COLLATE pg_catalog."default",
+    CONSTRAINT clients_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.clients
+    OWNER to postgres;
+    
+-- Table: public.client_contacts
+
+-- DROP TABLE IF EXISTS public.client_contacts;
+
+CREATE TABLE IF NOT EXISTS public.client_contacts
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 399999999 CACHE 1 ),
+    client_id bigint,
+    contact_type character varying(255) COLLATE pg_catalog."default",
+    contact_value character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT client_contacts_pkey PRIMARY KEY (id),
+    CONSTRAINT clients_ref_fkey FOREIGN KEY (client_id)
+        REFERENCES public.clients (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.client_contacts
+    OWNER to postgres;
+    
+
+
 -- Task 2. Part 1
 -- with contact_type
 select 
